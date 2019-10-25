0588ba2a8e48ef67f8fe97913871c5777ad3db09
Raphaël Delécluse <www.raphougi.w.c@gmail.com>
2019-10-25 22:13:12 +0200

    docpy
    Ajout des docs en python et test de kraken

---
+++ Shadoc-etudiant.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 17:18:29 2019
+
+@author: drira
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.linalg import null_space
+
+
+def stationnaire (P):
+  
+  
+    return res
+
+def stochastique (P):
+   
+   
+
+def puits (P,i):
+   
+
+
+
+def simulation(P, pi0, t0, tf):
+# Simulation numerique d'une chaine de Markov en temps discret
+# P	    : matrice de transition
+# pi0	: vecteur stochastique initial (a l'instant t0)
+# t0	: instant initial (debut de la simulation)
+# tf	: instant final
+# pi	: matrice des valeurs successives du vecteur stochastique
+# t     : liste des instants (t0 <= t <= tf)  
+
+    t = np.arange(t0,tf+1)
+# controles
+    if P.shape[0] != P.shape[1] | P.shape[0] != pi0.len :  
+        print('dimensions incorrectes')
+    elif not stochastique (P):
+        print ('la matrice n est pas stochastique')
+
+# evolution du vecteur stochastique
+    pi = np.array(np.zeros((len(t),P.shape[1])))
+    pi[0] = pi0  
+    for i in range(1,len(t)):
+        pi[i] = pi[i-1].dot(P)
+    plt.plot(t,pi)
+    return t,pi
+
+
+def Diagonalisation (P):
+   
+   
+
+# liste d'exemples de chaines de Markov
+def Exemple(n):
+   if n==1: 
+       print('shadock')
+       P = np.array([ [5/6 , 1/12, 1/12],  [ 1/4, 1/2, 1/4] , [ 1/4, 0, 3/4] ])
+       pi0 = [1 , 0, 0]
+   elif n==2:
+       print('imprimante')
+       P = np.array([ [.2, .8, 0],  [ .04, .95, .01] , [ .3, 0, .7] ])
+       pi0 = [1 , 0, 0]
+       
+       #  ajouter d'autres exemples
+       
+   return P,pi0
---
+++ projet-CM-Etudiant.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  4 15:47:08 2019
+
+@author: Hassen Drira
+"""
+
+from Shadock import Exemple
+from Shadock import Diagonalisation
+from Shadock import stationnaire
+from Shadock import simulation
+from Shadock import stochastique
+from puits import puits
+import numpy as np
+
+
+P = np.array([ [5/6 , 1/12, 1/12],  [ 1/4, 1/2, 1/4] , [ 1/4, 0, 3/4] ])
+pi0 = [1 , 0, 0]
+
+
+
+
+
+
+# Initialement en bonne santé
+pi0 = [1 , 0, 0]
+
+[t,pi] = simulation(P,pi0,1,100)
+print('pi[5]',pi[4])
+print('pi[10]',pi[9])
+print('pi[50]',pi[49])
+print('pi[100]',pi[99])
+
+# Initialisation differente
+pi0 = [0.4 , 0.2, 0.4]
+
+[t,pi] = simulation(P,pi0,1,100)
+print('pi[5]',pi[4])
+print('pi[10]',pi[9])
+print('pi[50]',pi[49])
+print('pi[100]',pi[99])
+
+# Initialisation differente
+pi0 = [0.1 , 0.4, 0.5]
+
+[t,pi] = simulation(P,pi0,1,100)
+print('pi[5]',pi[4])
+print('pi[10]',pi[9])
+print('pi[50]',pi[49])
+print('pi[100]',pi[99])
+
+#limite(pi)= [0.6 0.1 0.3]
+
+
+num = 2 # num entre 1 et 4
+P,pi0 = Exemple(num)
+print(P)
+
+
+
+
